3ef9da8919dd7975c0c1206b443623d800b90112
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-06-11 06:44:32 -0400

    commit tree 8dc4833898d07b31f1b41f8a90adf1f1d715f81d

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
-const titleCased = () => {
-  return tutorials
-}
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
+  
+  // const titleCased = () => {
+  //   return tutorials
+  // }
+  
+  function titleCased(){
+    let newTutorial=[];
+    for (let items of tutorials){
+  let capitalizeString = (string)=>string[0].toUpperCase()+
+    string.slice(1);
+  
+  let capitalizedWords =(string)=>string.split(' ').map
+    (capitalizeString).join(' ');
+  
+    newTutorial.push (capitalizedWords(items));
+      
+    }
+    return (newTutorial)
+  }
